9a0a2c1a0624fb69090a5a1a076ee44556d82682
andrew.kincaid <andrew.kincaid@servicenow.com>
2018-01-23 12:46:28 -0800

    test

--- test.js
+++ test.js
@@ -1,8 +1,6 @@
 render() {
-    const apos = '\'';
     return (
-        <h1>This couldn{apos}t be easier!</h1>
-        <h2>This isn't good</h2>
+        <h1>{t("This couldn't be easier")}</h1>
     );
 }
 
